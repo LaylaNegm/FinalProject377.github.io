6646f9d0405e1a9308a1bf5841de11cc8145956e
layla-N <112593484+LaylaNegm@users.noreply.github.com>
2022-12-06 17:21:45 -0500

    updated api url

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ function getRandomIntInclusive(min, max) {
   }
 
   async function getData() {
-    const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+    const url = 'https://data.princegeorgescountymd.gov/resource/vkdv-rvfx.json';
     const data = await fetch(url);
     const json = await data.json();
     const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name));
